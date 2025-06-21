3939155ac4126eab421923128c2d8791abe53ac3
arhamkac <arhamkachhara@gmail.com>
2025-06-21 15:04:57 +0530

    Footer size change

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 //Taken from Tailblocks
 function Footer() {
   return (
-    <footer className='bg-[#041922] p-10 flex flex-col items-center gap-5'>
+    <footer className='bg-[#041922] p-10 flex flex-col items-center gap-2'>
 
-    <h2 className='text-4xl text-amber-400 bg-blend-overlay w-fit underline decoration-amber-400 '>
+    <h2 className='text-3xl text-amber-400 bg-blend-overlay w-fit underline decoration-amber-400 '>
       <a href='https://github.com/arhamkac' target='_blank' rel="noopener noreferrer">
         Creator's GitHub
       </a>
