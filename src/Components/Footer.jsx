e3b614d6e6393d24b40b57815f8885f4410bd977
arhamkac <arhamkachhara@gmail.com>
2025-06-21 15:01:54 +0530

    Customized Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,8 +3,15 @@ import React from 'react';
 //Taken from Tailblocks
 function Footer() {
   return (
-    <footer className='bg-[#041922] p-10'>
-    <p className='flex justify-center items-center'>Copyright(c) 2025 MovieZone</p>
+    <footer className='bg-[#041922] p-10 flex flex-col items-center gap-5'>
+
+    <h2 className='text-4xl text-amber-400 bg-blend-overlay w-fit underline decoration-amber-400 '>
+      <a href='https://github.com/arhamkac' target='_blank' rel="noopener noreferrer">
+        Creator's GitHub
+      </a>
+    </h2>
+
+    <p>Copyright(c) 2025 MovieZone. All rights reserved</p>
     </footer>
   );
 }
